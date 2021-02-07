52769a3d55097d2b3c1f4e6a83869937d5fb88e8
Anu Mittal <anu22mittal@gmail.com>
2021-02-07 21:46:55 +0800

    Initial Commit

---
+++ TheFlagGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheFlagGame
+//
+//  Created by Anu Mittal on 07/02/21.
+//  Copyright © 2021 Anu Mittal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
